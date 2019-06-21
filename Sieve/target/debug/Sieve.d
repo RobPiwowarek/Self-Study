fd3e1b16c05cd3e5f2b37bc3cac9ca67c504e82f
Robert Piwowarek <rpw@touk.pl>
2019-06-21 16:13:46 +0200

    remove target

--- Sieve/target/debug/Sieve.d
+++
@@ -1,1 +0,0 @@
-/Users/robertpiwowarek/Desktop/Projects/Rust/Sieve/target/debug/Sieve.dSYM: /Users/robertpiwowarek/Desktop/Projects/Rust/Sieve/src/main.rs
--- Sieve/target/debug/deps/Sieve-7589def384dbe369.d
+++
@@ -1,5 +0,0 @@
-/Users/robertpiwowarek/Desktop/Projects/Rust/Sieve/target/debug/deps/Sieve-7589def384dbe369: src/main.rs
-
-/Users/robertpiwowarek/Desktop/Projects/Rust/Sieve/target/debug/deps/Sieve-7589def384dbe369.d: src/main.rs
-
-src/main.rs:
